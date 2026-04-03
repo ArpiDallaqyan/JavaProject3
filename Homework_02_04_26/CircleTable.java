722038e046b5241d75ed339468affb4fd0abc48e
Arpi Dallaqyan <arpidallaqyan15@gmail.com>
2026-04-03 21:50:36 +0400

    New Homework 03_04_26 Abstract Classes

---
+++ Homework_02_04_26/CircleTable.java
@@ -0,0 +1,18 @@
+package Homework_02_04_26;
+
+public class CircleTable extends Table{
+   private final double radius;
+    CircleTable(int numberOfTablesLegs, String tablesLegsMaterial, String tablesMaterial, double radius){
+        super(numberOfTablesLegs, tablesLegsMaterial, tablesMaterial);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    @Override
+    public double getArea() {
+        return 3.14 * (getRadius() * getRadius());
+    }
+}
